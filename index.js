3ebafd5d910b9e47496c7df3c8d22cbec7c7e9fe
Pawel Zimin <paweu14@gmail.com>
2021-10-28 15:14:55 +0200

    Some progress done

---
+++ index.js
@@ -0,0 +1,39 @@
+const tilesContainer = document.querySelector('.tiles-container');
+
+const getJson = async () => {
+   const response = await fetch('./projectData.json');
+   const jsonData = await response.json();
+   const data = jsonData.projectsData;
+   console.log(data);
+   return data;
+}
+
+const createProjectTile = (link, image, title) => {
+   const projectTile = document.createElement('div');
+   projectTile.classList.add('project-tile');
+   tilesContainer.appendChild(projectTile);
+
+   const a = document.createElement('a');
+   a.setAttribute('target', '_blank');
+   a.setAttribute('href', link);
+   projectTile.appendChild(a);
+
+   const img = document.createElement('img');
+   img.setAttribute('src', image);
+   a.appendChild(img);
+
+   const captionWrapper = document.createElement('p');
+   captionWrapper.classList.add('caption-wrapper');
+   captionWrapper.innerHTML = `<i class="fas fa-chevron-left"></i> ${title} <span>/</span><i class="fas fa-chevron-right"></i>`
+   a.appendChild(captionWrapper);
+}
+
+const renderTiles = async () => {
+   const data = await getJson();
+
+   for(const project in data){
+      createProjectTile(data[project].link, data[project].image, data[project].title);
+   }
+}
+
+renderTiles();
